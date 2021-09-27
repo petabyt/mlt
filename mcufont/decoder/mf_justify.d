277a06e1f0d410a8bc5274d2c3c3eb81e13b3e32
Daniel <brikbusters@gmail.com>
2021-09-27 17:49:32 -0400

    Manually merge mcufont branch

---
+++ mcufont/decoder/mf_justify.d
@@ -0,0 +1,4 @@
+mcufont/decoder/mf_justify.o: mcufont/decoder/mf_justify.c \
+ mcufont/decoder/mf_justify.h mcufont/decoder/mf_rlefont.h \
+ mcufont/decoder/mf_font.h mcufont/decoder/mf_encoding.h \
+ mcufont/decoder/mf_config.h mcufont/decoder/mf_kerning.h
---
+++ mcufont/decoder/mf_scaledfont.d
@@ -0,0 +1,3 @@
+mcufont/decoder/mf_scaledfont.o: mcufont/decoder/mf_scaledfont.c \
+ mcufont/decoder/mf_scaledfont.h mcufont/decoder/mf_font.h \
+ mcufont/decoder/mf_encoding.h mcufont/decoder/mf_config.h
