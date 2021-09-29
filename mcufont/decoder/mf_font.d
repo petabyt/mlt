5765874e3baa9b7d43450be8fad0493633b43fd0
Daniel <brikbusters@gmail.com>
2021-09-29 16:59:31 -0400

    Add spleen fonts

--- mcufont/decoder/mf_font.d
+++ mcufont/decoder/mf_font.d
@@ -1,8 +1,4 @@
 mcufont/decoder/mf_font.o: mcufont/decoder/mf_font.c \
  mcufont/decoder/mf_font.h mcufont/decoder/mf_encoding.h \
  mcufont/decoder/mf_config.h mcufont/fonts/fonts.h \
- mcufont/fonts/DejaVuSans12.c mcufont/decoder/mf_rlefont.h \
- mcufont/fonts/DejaVuSans12bw.c mcufont/fonts/DejaVuSerif16.c \
- mcufont/fonts/DejaVuSerif32.c mcufont/fonts/fixed_5x8.c \
- mcufont/decoder/mf_bwfont.h mcufont/fonts/fixed_7x14.c \
- mcufont/fonts/fixed_10x20.c mcufont/fonts/DejaVuSans12bw_bwfont.c
+ mcufont/fonts/fixed_10x20.c mcufont/decoder/mf_rlefont.h
